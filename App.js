a5d1f8ddaa295782bb2762f4f469c4f3380747c2
jkinathan <jkinobe@gmail.com>
2021-11-19 09:02:13 +0300

    fix: Fixes notification on App.js

--- App.js
+++ App.js
@@ -1,55 +1,32 @@
 // App.js
-import React from 'react';
-import PropTypes from 'prop-types';
-import { StyleSheet, View, StatusBar } from 'react-native';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+import React,{ Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { setLocalNotification } from './utils/notification';
+import store from './store'
+import HomeScreen from './components/HomeScreen'
 import { Provider } from 'react-redux';
-import reducer from './reducers/index';
-import Constants from 'expo-constants';
-import AppNavigator from './Navigation/AppNavigation';
-import { setLocalNotification } from './utils/helpers';
 
-
-const store = createStore(
-  reducer,
-  applyMiddleware(thunk, logger)
-);
-
-function FlashcardStatusBar({ backgroundColor, ...props }) {
-  return (
-    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
-      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
-    </View>
-  );
-}
-FlashcardStatusBar.propTypes = {
-  backgroundColor: PropTypes.string.isRequired
-};
-
-export default class App extends React.Component {
+export default class App extends Component {
+  
   componentDidMount() {
     setLocalNotification();
   }
+
   render() {
-    return (
-      <Provider store={store}>
-        <View style={styles.container}>
-          <FlashcardStatusBar
-            backgroundColor="green"
-            barStyle="light-content"
-          />
-          <AppNavigator />
-        </View>
-      </Provider>
-    );
+      return (
+          <Provider store={store}>
+              <View style={styles.container}>
+                <HomeScreen />
+              </View>
+          </Provider>
+      );
   }
+
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#dde'
+      flex: 1,
+      backgroundColor: '#dde'
   }
 });
